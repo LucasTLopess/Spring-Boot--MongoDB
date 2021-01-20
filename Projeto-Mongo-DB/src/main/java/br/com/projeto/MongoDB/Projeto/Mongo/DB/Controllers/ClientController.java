6bb86e5e8d6e721cc70057b9c69e5fd4752ca02e
[Lucas T.Lopes] <[troleizlucas@gmail.com]>
2021-01-20 15:05:44 -0300

    Criando Log's em controller

--- Projeto-Mongo-DB/src/main/java/br/com/projeto/MongoDB/Projeto/Mongo/DB/Controllers/ClientController.java
+++ Projeto-Mongo-DB/src/main/java/br/com/projeto/MongoDB/Projeto/Mongo/DB/Controllers/ClientController.java
@@ -3,6 +3,8 @@ package br.com.projeto.MongoDB.Projeto.Mongo.DB.Controllers;
 
 import br.com.projeto.MongoDB.Projeto.Mongo.DB.Entities.ClientEntity;
 import br.com.projeto.MongoDB.Projeto.Mongo.DB.Repositories.ClientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +19,11 @@ public class ClientController {
     @Autowired
     ClientRepository repository;
 
+    Logger logger = LoggerFactory.getLogger(ClientController.class);
+
     @GetMapping
     public ResponseEntity<?> list(){
+        logger.info("Acesando serviço para listar todos os clients");
         List<ClientEntity> list = repository.findAll();
         return ResponseEntity.ok(list);
     }
@@ -26,12 +31,15 @@ public class ClientController {
     @GetMapping(path="/{cnpj}")
     public ResponseEntity<ClientEntity> FindByRazaoSocial(@PathVariable String cnpj){
 
+        logger.info("Acessando Clint CNPJ: "+ cnpj);
         ClientEntity clientEntity = repository.findByrazaoSocial(cnpj);
 
         if(Objects.isNull(clientEntity)){
+            logger.info("CNPJ:"+ cnpj +" não está cadastrado na nossa base de dados: ");
             return ResponseEntity.noContent().build();
         }
         else{
+            logger.info("Apresenta Client CNPJ: " + cnpj );
             return ResponseEntity.ok(clientEntity);
         }
 
@@ -40,8 +48,10 @@ public class ClientController {
     @PostMapping
     public ResponseEntity<Object> saveClient(@RequestBody ClientEntity client){
        if (Objects.isNull(repository.findByrazaoSocial(client.getRazaoSocial()))){
+           logger.error("   ");
            return ResponseEntity.ok(repository.save(client));
        }else {
+           logger.error("Não foi possivel salvar Client CNPJ: " + client.getRazaoSocial());
            return ResponseEntity.badRequest().body(client);
        }
     }
